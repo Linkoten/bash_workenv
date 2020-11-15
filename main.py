8dfc24fb40aa23540a6e17d673e00218d52d563c
Paul <paul@debian.paul>
2020-11-16 00:56:15 +0100

    end parser, set up command run

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import typer
 import os
+import time
 from parser import parse_value
 
 def run_bin(bin: str, args: str):
     os.system(f"{bin} {args}")
-
+    
 def wmctrl(args):
     run_bin("wmctrl", args)
 
@@ -14,9 +15,12 @@ def go_to_desktop(desktop):
 def set_desktop_number(number: int):
     wmctrl(f"-n {number}")
 
-def main(name: str):
-    typer.echo(f"Hello {name}")
-    parse_value("test")
+def main(workenv_name: str):
+    workenv = parse_value(workenv_name)
+    for workenv_command in workenv.commands:
+        go_to_desktop(workenv_command.desktop)
+        run_bin(workenv_command.command, "")
+        time.sleep(2)
 
 if __name__ == "__main__":
     typer.run(main)
--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 import yaml
+from pydantic import BaseModel
+from typing import List
 
 # Config file path.
 CONFIG_PATH = ".workenv.yml"
@@ -8,8 +10,21 @@ DESKTOP_NUMBER = "desktop_number"
 DEFAULT_DESKTOP = "default_desktop"
 COMMANDS = "commands"
 
+class WorkenvCommand(BaseModel):
+    command: str
+    desktop: int
+
+class Workenv(BaseModel):
+    desktop_number: int
+    default_desktop: int
+    commands: List[WorkenvCommand]
+
 def parse_value(name):
     with open(CONFIG_PATH, 'r') as config_file:
         configs = yaml.safe_load(config_file)
     workenv_config = configs[name]
-
+    commands = []
+    for desktop, command in workenv_config[COMMANDS]:
+        commands.append(WorkenvCommand(command = command, desktop = desktop))
+    workenv = Workenv(desktop_number = workenv_config[DESKTOP_NUMBER], default_desktop = workenv_config[DEFAULT_DESKTOP], commands = commands)
+    return workenv
